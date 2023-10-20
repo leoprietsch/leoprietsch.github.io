09eef62b93e962192ba0724442566c7dd52b91f3
leoprietsch <leo.prietsch@gmail.com>
2023-10-19 21:28:52 -0300

    style: reset css add colors and Mono font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        moss: "#606C38",
+        pakistan: "#283618",
+        cornsilk: "#FEFAE0",
+      },
+    },
   },
   plugins: [],
 };
