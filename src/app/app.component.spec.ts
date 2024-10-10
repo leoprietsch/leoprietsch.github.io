374ea11b392e255660b813c80c48eb75635ba3c3
leoprietsch <leo.prietsch@gmail.com>
2024-10-10 10:27:50 -0300

    refactor: revert project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'personal-website' title`, () => {
+  it(`should have the 'my-website' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('personal-website');
+    expect(app.title).toEqual('my-website');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, personal-website');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my-website');
   });
 });
